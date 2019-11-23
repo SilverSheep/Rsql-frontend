1a00d82bdd5374c741dfb7dd8e9e6e8410900f63
Marcin Baranowski <m.baranowskimarcin@gmail.com>
2019-11-23 21:46:31 +0100

    Adds base of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimalDetailsComponent } from './animal-details/animal-details.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalDetailsComponent,
+    AnimalListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
